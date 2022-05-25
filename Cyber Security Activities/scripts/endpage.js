0b9e08c38ac31488b8c38cf6ae55ced4edb72a0e
shaailpatel <87975469+shaailpatel@users.noreply.github.com>
2022-05-25 14:17:46 +1000

    Delete Cyber Security Activities directory

--- Cyber Security Activities/scripts/endpage.js
+++
@@ -1,6 +0,0 @@
-var mailscore = localStroage.getItem("username");
-var smscore = localStroage.getItem("scoresm");
-
-function checkscore() {
-    document.getElementById("scoreofmail").innerHTML = mailscore;
-}
